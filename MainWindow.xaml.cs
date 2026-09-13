98e23d19675fe15223b6ab11de4268dddaf83c88
Abhiram Anil <abhiram.anil.0519@gmail.com>
2026-09-13 09:56:07 +0530

    Organized project structure

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -5,9 +5,12 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading.Tasks;
+using thunderfield_wingman.Methods;
 
 namespace thunderfield_wingman;
 
@@ -19,5 +22,8 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+
+        WingmanUI.StartBootAnim(MainFrame);
+
     }
 }
---
+++ Methods/WingmanUI.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media.Animation;
+using thunderfield_wingman.Pages;
+
+namespace thunderfield_wingman.Methods
+{
+    public class WingmanUI
+    {
+        // Boot Animation
+        public static async void StartBootAnim(Frame mainFrame)
+        {
+            await Task.Delay(1000);
+
+            mainFrame.Opacity = 0;
+
+            mainFrame.Navigate(new Boot());
+
+            var fadeIn = new DoubleAnimation
+            {
+                From = 0,
+                To = 1,
+                Duration = TimeSpan.FromSeconds(0.7)
+            };
+
+            mainFrame.BeginAnimation(
+                System.Windows.UIElement.OpacityProperty,
+                fadeIn);
+        }
+    }
+
+}
